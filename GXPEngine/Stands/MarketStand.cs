f8feb16bd2ccaefa9820c4fdc59df5315472abe1
Maurice Johannssen <476655@student.saxion.nl>
2020-05-11 10:45:21 +0200

    Small additions to MarketStand class

--- GXPEngine/Stands/MarketStand.cs
+++ GXPEngine/Stands/MarketStand.cs
@@ -31,11 +31,10 @@ public class MarketStand : AnimationSprite
     {
         Vec2 hoverDistanceMouseCharacter = new Vec2((_mouseX + LevelLoader.character.CalculateCharacterOffset().x) - LevelLoader.character.Position.x, (_mouseY + LevelLoader.character.CalculateCharacterOffset().y) - LevelLoader.character.Position.y);
 
-        if (_mouseX >= (x - LevelLoader.character.CalculateCharacterOffset().x) - width / 2 && _mouseX <= (x - LevelLoader.character.CalculateCharacterOffset().x) + width / 2 
-            && _mouseY >= (y - LevelLoader.character.CalculateCharacterOffset().y) - height / 2 
+        if (_mouseX >= (x - LevelLoader.character.CalculateCharacterOffset().x) - width / 2 && _mouseX <= (x - LevelLoader.character.CalculateCharacterOffset().x) + width / 2
+            && _mouseY >= (y - LevelLoader.character.CalculateCharacterOffset().y) - height / 2
             && _mouseY <= (y - LevelLoader.character.CalculateCharacterOffset().y) + height / 2 && hoverDistanceMouseCharacter.Length() < 300) SetFrame(1);
         else SetFrame(0);
-        Console.WriteLine(hoverDistanceMouseCharacter.Length());
     }
 
     protected void Update()
