782d449cb731fbb5fc38cff132f98f2ef2b145ac
TirillPedersen <59061304+TirillPedersen@users.noreply.github.com>
2020-05-14 12:17:24 +0200

    pigeonss

---
+++ GXPEngine/Pigeon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GXPEngine;
+
+public class Pigeon : AnimationSprite
+{
+    public Pigeon() : base("pigeon.png", 2, 3)
+    {
+        SetXY(500,500);
+    }
+}
+
