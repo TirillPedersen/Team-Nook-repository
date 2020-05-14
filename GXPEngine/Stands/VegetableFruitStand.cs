d3b4303c39916a460317341e782c7d8c804b4110
Maurice Johannssen <476655@student.saxion.nl>
2020-05-14 11:19:07 +0200

    Forgot one class lol

--- GXPEngine/Stands/VegetableFruitStand.cs
+++ GXPEngine/Stands/VegetableFruitStand.cs
@@ -7,8 +7,37 @@ namespace GXPEngine
 {
     class VegetableFruitStand : MarketStand
     {
+        private Sprite _buyMenu;
+        private bool _menuShown;
+
         public VegetableFruitStand(float givenX, float givenY, float givenRotation = 0) : base(givenX, givenY, "VegetableFruitsStand.png", givenRotation)
-        { 
+        {
+            _buyMenu = new Sprite("vegetableBuyScreen.png");
+            _buyMenu.SetOrigin(_buyMenu.width / 2, _buyMenu.height / 2);
+            _buyMenu.SetXY(game.width - _buyMenu.width, game.height - _buyMenu.height);
+
+            _menuShown = false;
+        }
+
+        private void BuyMenu()
+        {
+            if (hoveringOverStand && Input.GetMouseButtonDown(0) && !_menuShown)
+            {
+                LevelLoader.hud.AddChild(_buyMenu);
+                _menuShown = true;
+            }
+            else if (hoveringOverStand && _menuShown && Input.GetMouseButtonDown(0))
+            {
+                LevelLoader.hud.RemoveChild(_buyMenu);
+                _menuShown = false;
+            }
+        }
+
+        protected void Update()
+        {
+            getMousePos();
+            mouseHover();
+            BuyMenu();
         }
     }
 }
